ad5b0ec89df2f969907cc25eab76e5ec16c218be
laymancode <jaykumawat969@gmail.com>
2020-06-22 18:49:23 +0530

    Update ExportQuery.php

--- ExportQuery.php
+++ ExportQuery.php
@@ -30,9 +30,9 @@ class ExportQuery {
 			$host = "localhost";
 			$port = 3306;
 			$socket = "";
-			$user = "ttslrf";
-			$password = "ttslrf123";
-			$dbname = "tcts";
+			$user = "user";
+			$password = "pass";
+			$dbname = "db_name";
 			$con = new mysqli($host, $user, $password, $dbname, $port, $socket) or die('Could not connect to the database server' . mysqli_connect_error());
 			$this->con = $con;	
 		}
